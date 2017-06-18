1e50b4647eaf33ec661bb6890ca72a095d17ec2f
Brad Lundy <crusin67@hotmail.com>
2017-06-17 21:59:08 -0700

    initial commit to web

---
+++ Script/taskJS.js
@@ -0,0 +1,15 @@
+﻿function CreateTask() {
+    console.log("In CreateTask function!");
+
+    var name = document.getElementsByName('person');
+    var difficult = document.getElementsByName('difficulty');
+    var description = document.getElementById('task');
+
+    var parent = document.getElementById("list-container");
+    var ul = document.createElement("ul");
+    var li = document.createElement('li');
+
+    li.innerHTML = name + "has a task of" + description + "(" + difficult + ").";
+    ul.appendChild(li);
+
+}

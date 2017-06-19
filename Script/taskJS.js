d5125d3619f72a3cf2e50dd9069f30687e3c9257
Brad Lundy <crusin67@hotmail.com>
2017-06-18 21:42:04 -0700

    structure js file

--- Script/taskJS.js
+++ Script/taskJS.js
@@ -1,18 +1,19 @@
 ﻿function CreateTask() {
     event.preventDefault();
     console.log("In CreateTask function!");
-
-
+    
+    //get values from the form
     var name = document.querySelector('input[name="person"]:checked').value;
     var difficult = document.getElementById('complexity').value;
     var description = document.getElementById('task').value;
 
+    //create the ul and li and know where to place it in html body
     var parent = document.getElementById("list-container");
     var ulNode = document.createElement("ul");
-
     var liNode = document.createElement('li');
-    var liText = document.createTextNode(name + " has a task of " + description + ", (" + difficult + ").");
 
+    //write to div in html
+    var liText = document.createTextNode(name + " has a task of " + description + ", (" + difficult + ").");
     liNode.appendChild(liText);
     ulNode.appendChild(liNode);
     parent.appendChild(ulNode);
